8b1d1062d8fda4e0d031ee12443266e43a8b2ad9
simonvic <simonvic.dev@gmail.com>
2026-06-04 00:10:52 +0200

    feat(queries/aerial): capture labeled alternatives as enum members

--- queries/aerial.scm
+++ queries/aerial.scm
@@ -3,10 +3,19 @@
     (identifier) @name)
   (#set! "kind" "Class")) @symbol
 
+(rule_spec_lexer
+  (token_ref) @name
+  (#set! "kind" "Function")) @symbol
+
 (rule_spec_parser
   (rule_ref) @name
   (#set! "kind" "Function")) @symbol
 
-(rule_spec_lexer
-  (token_ref) @name
-  (#set! "kind" "Function")) @symbol
+(rule_spec_parser
+  (rule_ref) @name
+  (rule_block
+    (rule_alt_list
+      (labeled_alt
+        (identifier) @name
+        (#set! "kind" "EnumMember")) @symbol))
+  (#set! "kind" "Enum")) @symbol
